bc555dc125d49de4e22bf18268f6d6caa172088e
Kostya27Kaz <email@example.ru>
2022-11-04 11:20:42 +1000

    unit2

--- project_0/My_game.py
+++ project_0/My_game.py
@@ -1,14 +1,3 @@
 import numpy as np
-
-def random_predict(number:int=1) -> int:
-    
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101) 
-        if number == predict_number:
-            break 
-    return(count)
-
-print(f'Количество попыток: {random_predict()}')
+a = np.int64(270)
+print(np.iinfo(a))
