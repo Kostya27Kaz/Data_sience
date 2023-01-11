5dcfc5a6d21233df77543875063ab90c9c03f503
Kostya27Kaz <email@example.ru>
2023-01-11 14:25:27 +1000

    new one commit

--- project_0/My_game.py
+++ project_0/My_game.py
@@ -1,5 +1,15 @@
-import numpy as np
-workers = ['Ivan', 'Nikita', 'Maria', 'John', 'Kate']
- 
-choice = np.random.choice(workers, size=2, replace=False)
-print(choice)
+parol = {'Olga':'Дзержинского'}
+
+while True:
+    login = input('Введите логин: ')
+    if login in parol:
+        password = input('Введите пароль: ')
+        if password == parol[login]:
+            print ('Welcome')
+            break
+        else:
+            print('Неправильный пароль')
+    else:
+        print('Неправильный логин')
+            
+
